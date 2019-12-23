72d14d05406308cd6b5e89332ef7f4f45da77a2f
arnizzle <apolman@fastmail.fm>
2019-12-23 14:36:59 +0100

    redis node update

--- NodeRedis/index.js
+++ NodeRedis/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
+
 const app = express();
 const client = redis.createClient( {
     host: 'redis',
     port: 6379
 });
 
-
-
 client.set('visits', 0)
 
 app.get('/', (req, res) =>{
+    
     client.get('visits', (err, visits) => {
         res.send('Number of visits: ' + visits);
         client.set('visits', parseInt(visits) + 1)
+        if (parseInt(visits) > 10 ) {
+            process.exit(0);
+        }
     })
+
 })
 
 app.listen(8081, () =>{
